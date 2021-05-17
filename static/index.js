7a081484448e8e3bd6730f1d56b42a2b08c51c62
YUUKIToriyama <YUUKIToriyama@users.noreply.github.com>
2021-05-17 08:02:57 +0000

    deploy: 9fd87297042418d65fb1a2e60ec84fd02f502bbe

--- static/index.js
+++ static/index.js
@@ -3,3 +3,4 @@ import ReactDOM from "../_snowpack/pkg/react-dom.js";
 import "./style.css.proxy.js";
 import App from "./App.js";
 ReactDOM.render(/* @__PURE__ */ React.createElement(App, null), document.getElementById("app"));
+//# sourceMappingURL=data:application/json;base64,ewogICJ2ZXJzaW9uIjogMywKICAic291cmNlcyI6IFsiL2hvbWUvcnVubmVyL3dvcmsvaW1hZ2VDb21wYXJlL2ltYWdlQ29tcGFyZS9zcmMvaW5kZXguanN4Il0sCiAgIm1hcHBpbmdzIjogIkFBR0E7QUFDQTtBQUdBO0FBR0E7QUFDQSxTQUFTLE9BQU8sb0NBQUMsS0FBRCxPQUFTLFNBQVMsZUFBZTsiLAogICJuYW1lcyI6IFtdCn0K
