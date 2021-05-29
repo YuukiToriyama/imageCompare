c6b044380fe90571f4e0deec282f0b395ce3c8cc
YUUKIToriyama <ytoriyama@torisfactory.com>
2021-05-30 05:09:33 +0900

    npm install license-report

--- src/components/block/LicenseInfo.jsx
+++ src/components/block/LicenseInfo.jsx
@@ -14,24 +14,17 @@ import licenses from "../../licenses.json";
 const LicenseInfo = () => {
 	return (
 		<List>
-			{Object.keys(licenses).map((packageName) => {
+			{licenses.map((license) => {
+				const abstract = license["name"] + "@" + license["installedVersion"];
 				return (
 					<React.Fragment>
 						<ListItem alignItems="flex-start">
 							<ListItemText
-								primary={packageName}
+								primary={abstract}
 								secondary={
 									<React.Fragment>
-										{licenses[packageName]["publisher"] != undefined ?
-											<Typography
-												component="p"
-												variant="body2"
-												color="textPrimary"
-											>
-												by {licenses[packageName]["publisher"]}
-											</Typography>
-											: ""}
-										{licenses[packageName]["licenseText"]}
+										<Typography component="p" variant="body2" color="textPrimary">{license["licenseType"] + " by " + license["author"]}</Typography>
+										<Typography component="p" variant="body2" color="textSecondary">{license["link"]}</Typography>
 									</React.Fragment>
 								}
 							/>
